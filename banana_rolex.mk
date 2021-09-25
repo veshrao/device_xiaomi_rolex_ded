3a04369b59b792c06b1d77a6abf7d6de6c4b4451
vesh <veshrao@protonmail.com>
2021-09-25 20:49:43 +0800

    Revert "rova: added/integrated gapps"
    This reverts commit f9fda3aff237e3ad54d3ec45890cfeb170727ab2.

--- banana_rolex.mk
+++ banana_rolex.mk
@@ -22,8 +22,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common BananaDroid stuff
 $(call inherit-product, vendor/banana/config/common.mk)
-GAPPS_VERSION := true
-TARGET_GAPPS_ARCH := arm64
 
 # Inherit from rolex device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
